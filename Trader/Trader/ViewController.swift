616736d18a5a7a93f51d742e16d6bfcc566d695c
ahmettuztasi <tuztasiahmet@gmail.com>
2020-05-01 18:51:02 +0300

    project created

---
+++ Trader/Trader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trader
+//
+//  Created by admin on 1.05.2020.
+//  Copyright © 2020 ahmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
